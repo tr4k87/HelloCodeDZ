aae86aa7759efaa383cf6a40dd283f80866ecfcc
Sergey <tr4k@mail.ru>
2022-08-05 23:48:16 +0300

    add code Example3

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение ");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0 )
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
