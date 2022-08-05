080fddb80597f768b0ce070f6729cc75d6150fa4
Sergey <tr4k@mail.ru>
2022-08-05 23:34:20 +0300

    add code Example2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c =Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.Write("Максимальное значение ");
+Console.WriteLine(max);
