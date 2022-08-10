6b8e812bc40581b8f4b622a19a87c9a435769bbc
Sergey <tr4k@mail.ru>
2022-08-11 00:44:49 +0300

    add code seminar2_3

--- Seminar2_3/Program.cs
+++ Seminar2_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Выберите день недели");
 Console.WriteLine("1-Понедельник, 2-Вторник, 3-Среда, 4-Четверг, 5-Пятница, 6-Суббота, 7-Воскресенье");
 int day = Convert.ToInt32(Console.ReadLine());
-if (day>5)
+if (day > 5)
 {
     Console.WriteLine("ДА!!!! Сегодня выходной");
 }
