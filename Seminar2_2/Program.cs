1c549304e80541262fc8348bd93f1f9750070d2d
Sergey <tr4k@mail.ru>
2022-08-11 00:24:17 +0300

    add code seminar2_2

---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string chis = Console.ReadLine();
+if (chis.Length == 3)
+{
+    Console.WriteLine(chis[2]);
+}
+else
+{
+    Console.WriteLine("Нет третьего числа");
+}
