9e637fbfc4997557cebbe1db340fe5178613c468
Sergey <tr4k@mail.ru>
2022-08-14 21:53:32 +0300

    add seminar3_1

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string pal = Console.ReadLine();
+if (pal[0] == pal[4])
+{
+    if (pal[1] == pal[3])
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("нет");
+}
