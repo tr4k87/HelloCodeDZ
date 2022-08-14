17ab2237fc2c5116b226a1bdf5b961a3ab77afc3
Sergey <tr4k@mail.ru>
2022-08-14 23:28:05 +0300

    add Seminar3_2

---
+++ Seminar3_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите координаты");
+var x1 = Convert.ToInt32(Console.ReadLine());
+var y1 = Convert.ToInt32(Console.ReadLine());
+var z1 = Convert.ToInt32(Console.ReadLine());
+var x2 = Convert.ToInt32(Console.ReadLine());
+var y2 = Convert.ToInt32(Console.ReadLine());
+var z2 = Convert.ToInt32(Console.ReadLine());
+var rast = Math.Sqrt((x2 - x1)*(x2-x1) + (y2 - y1)*(y2-y1) + (z2 - z1)*(z2-z1));
+Console.WriteLine(Math.Round(rast, 2));
