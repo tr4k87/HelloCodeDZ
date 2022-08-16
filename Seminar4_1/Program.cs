54f7341a0aa2cc0d289e15d75d0e58ac725c9326
Sergey <tr4k@mail.ru>
2022-08-16 22:49:49 +0300

    add Seminar4_1

---
+++ Seminar4_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите значения");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+for (int i = 0; i < b; i++)
+{
+   c = c * a;
+}
+Console.WriteLine($"Натуральная степень 2х чисел = {c}");
