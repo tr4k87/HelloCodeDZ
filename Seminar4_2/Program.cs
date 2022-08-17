c0e39e835c755d881c8669de0d754c089fe6aac8
Sergey <tr4k@mail.ru>
2022-08-17 18:08:02 +0300

    add Seminar4_2

---
+++ Seminar4_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите значение");
+string str = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < str.Length; i++)
+{   
+    sum = sum + Convert.ToInt32(str[i] - '0');
+}
+Console.WriteLine(sum);
