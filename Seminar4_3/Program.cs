0821733e8a69a592d6f6cc97d5d93516d1ddac23
Sergey <tr4k@mail.ru>
2022-08-17 22:58:57 +0300

    add seminar4_3

---
+++ Seminar4_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Значения массива");
+string mass = Console.ReadLine();
+int j = mass.Length;
+string[] array;
+array = new string[j];
+Console.Write("[");
+for (int i = 0; i < j; i++)
+{
+  Console.Write(mass[i] + ",");
+}
+Console.Write("]");
+
+
+
+
