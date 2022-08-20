9db0fada5c329c6efa67e99d1922526b63789b46
Sergey <tr4k@mail.ru>
2022-08-20 23:16:35 +0300

    add Seminar5_2

---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static int[] AddArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array)
+    {
+        int multi = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            multi = array[i] * array[array.Length - 1 - i];
+            Console.Write(multi);
+        }
+    }
+    static void Main(string[] argm)
+    {
+        int[] array = AddArray(5);
+        WriteArray(array);
+    }
+}
+
