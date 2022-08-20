2351cb2f23532d9e9e88e2aa6beb006bf6750bd7
Sergey <tr4k@mail.ru>
2022-08-20 22:36:03 +0300

    add Seminar5_1

---
+++ Seminar5_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static int[] CreaterArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    static int[] RandomArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int j = new Random().Next(0, i+1);
+            int number = array[j];
+            array[j] = array[i];
+            array[i] = number;
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]>10)
+                {
+                    if (array[i] < 99)
+                    {
+                        Console.Write(array[i] + ",");  
+                    }
+                }
+            }   
+        Console.WriteLine();
+    }
+    static void Main (string[] argm)
+    {
+        int[] array = CreaterArray(123);
+        array = RandomArray(array);
+        WriteArray(array);
+    }
+}
