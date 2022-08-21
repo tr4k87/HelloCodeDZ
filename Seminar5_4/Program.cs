b07cffd9d69540c6c115ee08cbed87a8aad2f983
Sergey <tr4k@mail.ru>
2022-08-21 22:20:34 +0300

    add Seminar5_4

---
+++ Seminar5_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] number = new int[10];
+Random random = new Random();
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    Console.Write((number[i] = random.Next(100, 999)) + ",");
+
+    if (number[i] % 2 == 0)
+    {
+        sum = sum + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(sum);
