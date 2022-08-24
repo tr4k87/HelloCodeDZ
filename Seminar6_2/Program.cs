be97177ddff5b3a4fd5d5a56f17f74bdbe7fe73e
Sergey <tr4k@mail.ru>
2022-08-24 13:49:19 +0300

    add Seminar6_2

---
+++ Seminar6_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите значения b1, k1, b2, k2");
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+var x = (b2 - b1) / (k1 - k2);
+var y = (k1*b2 - k2*b1) / (k1 - k2);
+Console.WriteLine($"Точка пересечния: [{x}], [{y}]");
