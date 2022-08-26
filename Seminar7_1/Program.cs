17c940ad81580ad647ae0cb315b4f6669f4e29e6
Sergey <tr4k@mail.ru>
2022-08-26 22:28:48 +0300

    correct style

--- Seminar7_1/Program.cs
+++ Seminar7_1/Program.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 class Program
 {
-static void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+    static void PrintArray(int[,] array)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            Console.Write($"{array[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-static int[,] RandomArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i,j] = new Random().Next(1,100);
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
         }
     }
-    return array;
-}
-static void Main(string[] argm)
-{
-int[,] array = new int[3,4];
-PrintArray(array);
-RandomArray(array);
-PrintArray(array);
-}
+    static int[,] RandomArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(1, 100);
+            }
+        }
+        return array;
+    }
+    static void Main(string[] argm)
+    {
+        int[,] array = new int[3, 4];
+        PrintArray(array);
+        RandomArray(array);
+        PrintArray(array);
+    }
 }
