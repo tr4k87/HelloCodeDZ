cd9eb10d2d164a370c352e12468c2521b10f74eb
Sergey <tr4k@mail.ru>
2022-08-28 12:38:15 +0300

    add Seminar7_3

---
+++ Seminar7_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Program
+{
+    static void WriteArray(int[,] array)
+    {
+        Console.Clear();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static int[,] RandomArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(0, 10);
+            }
+        }
+        return array;
+    }
+    static void Sred(int[,] array)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum = sum + array[i,j];
+            }
+            Console.Write($"{Math.Round(sum/3, 1)} ");
+            sum = 0;
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] argm)
+    {
+        
+        int[,] array = new int[3, 4];
+        RandomArray(array);
+        WriteArray(array);
+        Console.WriteLine();
+        Sred(array);
+    }
+}
