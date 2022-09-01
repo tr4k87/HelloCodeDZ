149386de23fb27ffa8103bf432d6a202eef3d462
Sergey <tr4k@mail.ru>
2022-09-01 11:52:24 +0300

    add Seminar8.1

---
+++ Seminar8_1/Program.cs
@@ -0,0 +1,49 @@
+﻿static int[,] RandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 99);
+        }
+    }
+    return array;
+}
+static void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+static int[,] Sort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int s = 0; s < array.GetLength(1) - 1; s++)
+            {
+                if (array[i, s] < array[i, s + 1])
+                {
+                    int temp = array[i, s + 1];
+                    array[i, s + 1] = array[i, s];
+                    array[i, s] = temp;
+                }
+            }
+        }
+    }
+    Console.WriteLine();
+    return array;
+}
+
+int[,] array = new int[4, 4];
+RandomArray(array);
+WriteArray(array);
+Sort(array);
+WriteArray(array);
