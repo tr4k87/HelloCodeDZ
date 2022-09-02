ec54b2a3ca2c9faebab92e8129d7797755ed0b09
Sergey <tr4k@mail.ru>
2022-09-02 22:22:27 +0300

    add Seminar8_5

---
+++ Seminar8_5/Program.cs
@@ -0,0 +1,39 @@
+﻿
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+static int[,] AddArray(int[,] array)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+   while (temp <= array.GetLength(0) * array.GetLength(1))
+{
+  array[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > array.GetLength(1) - 1)
+    j--;
+  else 
+    i--;
+}
+    return array;
+}
+
+
+int[,] array = new int[4,4];
+PrintArray(array);
+AddArray(array);
+Console.WriteLine();
+PrintArray(array);
