2dfde7a0002b3118f9d3fa55f6d8ab342429ad9d
Sergey <tr4k@mail.ru>
2022-09-03 22:15:33 +0300

    add Seminar9_1

---
+++ Seminar9_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите значение М");
+int M = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int N = Int32.Parse(Console.ReadLine());
+int Sum(int M)
+{
+    int sum = 0;
+    if (M == N) return N;
+    return M + Sum(M + 1);
+}
+Console.Write(Sum(M));
