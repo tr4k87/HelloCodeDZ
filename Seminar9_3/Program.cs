c6728dc4ed23a8e04a71c1fea685d30f66c74196
Sergey <tr4k@mail.ru>
2022-09-03 23:34:13 +0300

    add Seminar9_3

---
+++ Seminar9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значение М");
+int M = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int N = Int32.Parse(Console.ReadLine());
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M > 0 && N == 0) return Akkerman(M - 1, 1);
+    if (M > 0 && N > 0) return Akkerman(M - 1, Akkerman(M, N - 1));
+    return Akkerman(M , N);
+}
+Console.WriteLine(Akkerman(M, N));
