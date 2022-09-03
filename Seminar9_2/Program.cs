11896fb9b161a8a44e61ea7293c15cf887aa786a
Sergey <tr4k@mail.ru>
2022-09-03 23:15:42 +0300

    add Seminar9_2

---
+++ Seminar9_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Введите значение М");
+int M = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int N = Int32.Parse(Console.ReadLine());
+void Nat(int M)
+{
+    if (M > N) return;
+    Console.Write(M + " ");
+    Nat(M + 1);
+}
+Nat(M);
