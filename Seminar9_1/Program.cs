fea36ef895fa1a959735efdf5ff71ab9ee8064fd
Sergey <tr4k@mail.ru>
2022-09-03 22:18:15 +0300

    add Seminar9_2

--- Seminar9_1/Program.cs
+++ Seminar9_1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите значение М");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите значение М");
 int M = Int32.Parse(Console.ReadLine());
 Console.WriteLine("Введите значение N");
 int N = Int32.Parse(Console.ReadLine());
